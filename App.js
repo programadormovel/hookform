5cf6f60b7a7529c4b9b3eab8dec758e1c91b15bc
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2022-08-11 21:22:57 -0300

    Aplicado o estilo no container e input

--- App.js
+++ App.js
@@ -6,10 +6,33 @@ import { useForm, Controller } from 'react-hook-form';
 export default function HookForm() {
 
   return (
-    <View>
-      <TextInput value = {"nome"} />
-      <TextInput value={'sobrenome'} />
+    <View style={styles.container}>
+      <TextInput 
+        style={styles.input}
+        value = {"nome"} />
+      
+      <TextInput 
+        style={styles.input}
+        value={'sobrenome'} />
+
       <Button title="Enviar Dados" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: "#ccc",
+    margin: 8,
+    padding: 8,
+    minWidth: '70%',
+    borderRadius: 8,
+  }
+})
