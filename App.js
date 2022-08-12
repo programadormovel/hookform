4a6ba10bf76cea74a9a22478d226e586b711eab8
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2022-08-11 21:32:46 -0300

    inserido o Controller no input do nome

--- App.js
+++ App.js
@@ -5,12 +5,42 @@ import { useForm, Controller } from 'react-hook-form';
 
 export default function HookForm() {
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      nome: "",
+      sobrenome: "",
+    }
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    Alert.alert(data.nome + "\n" + 
+      data.sobrenome);
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput 
-        style={styles.input}
-        value = {"nome"} />
-      
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, onBlur, value }}) => (
+          <TextInput 
+            style={styles.input}
+            value = {value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            placeholder="Digite seu nome"
+          />
+        )}      
+      />
+
       <TextInput 
         style={styles.input}
         value={'sobrenome'} />
